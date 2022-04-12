b657fe0f28a6bc2995bc33926e17184c97f17095
Eduardo Miguel Victoria Sánchez <71092451+EduardoMiguelViSa@users.noreply.github.com>
2022-04-12 15:25:23 -0500

    Add ejercicio 2

---
+++ Tareas/Tarea3/index-2.js
@@ -0,0 +1,21 @@
+let hasta = 10;
+
+for (i = 0; i <= hasta; i++) {
+    switch (i) {
+        case 2:
+            console.log(" Número primo " + i + ".");
+            break;     
+        case 3:
+            console.log(" Número primo " + i + ".");
+            break;
+        case 5:
+            console.log(" Número primo " + i + ".");
+            break;
+        case 7:
+            console.log(" Número primo " + i + ".");
+            break;
+        default:
+            console.log(" Esta es la iteración número " + i + ".")
+            break;
+    }
+}
