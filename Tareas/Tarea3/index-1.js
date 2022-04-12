9172ed535c4a9be5549ad5a75371181f583502fe
Eduardo Miguel Victoria Sánchez <71092451+EduardoMiguelViSa@users.noreply.github.com>
2022-04-12 15:14:38 -0500

    Add Tarea 3 y ejercicio 1

---
+++ Tareas/Tarea3/index-1.js
@@ -0,0 +1,9 @@
+let hasta = 100;
+
+for (i = 1; i <= hasta; i++) {
+    if (i%2 === 0) {
+        console.log(" El número " + i + " es par.");
+    } else {
+        console.log(" El número " + i + " es impar.");
+    }
+}
